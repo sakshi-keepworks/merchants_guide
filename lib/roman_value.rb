b04c8c16f3df8f6225739de326abe4ad335401d2
sjain1107 <sakshijain1107@gmail.com>
2016-05-28 10:27:59 +0530

    More validations

--- lib/roman_value.rb
+++ lib/roman_value.rb
@@ -1,4 +1,3 @@
-require 'byebug'
 class RomanValue
   attr_accessor :test
 
@@ -8,7 +7,7 @@ class RomanValue
     ans = 0
     if valid_number?
       test.split("").each_with_index do |val,index|
-        next if test[index - 1] && check_precedence(test[index - 1],val)
+        next if !index.eql?(0) && check_precedence(test[index - 1],val)
         if test[index + 1] && check_precedence(val,test[index + 1])
           ans += ROMAN[test[index + 1].to_sym] - ROMAN[val.to_sym]
         else
@@ -26,6 +25,8 @@ class RomanValue
   def valid_number?
     return false if check_number_exist?(["I","X","C","M"],4)
     return false if check_repetition?(["D","L","V"])
+    return false if check_subtraction
+    return false if check_no_subtraction
     true
   end
 
@@ -35,6 +36,25 @@ class RomanValue
 
   def check_repetition?(arr)
     arr.detect {|num| test.each_char.find { test.count(num) > 1 }}
+  end
+
+  def check_subtraction
+    if check_number_exist?(["I","X","C"],1)
+      return true if test =~ /I[^X­VI]/
+      test =~ /X[^LCIX]/
+    else
+      false
+    end
+  end
+
+  def check_no_subtraction
+    if check_number_exist?(["D","L","V"],1)
+      return true if test =~ /V[^I]/
+      return true if test =~ /L[^XIV]/
+      test =~ /D[^IVXLC]/
+    else
+      false
+    end
   end
 
   def check_precedence(a,b)
